d88a3e55ea353b2a8407e1e95c2882b90bc7b086
dc <danni.co@outlook.fr>
2017-01-12 10:53:35 +0100

    Fixes #12 get all distinct values with 2 queries, 1 for the size, 1 for getting all

--- core/services/modules/distinct/Field.py
+++ core/services/modules/distinct/Field.py
@@ -22,19 +22,42 @@ class Field:
 		self.indexName = cfg.get('elasticsearch', 'indexNameES')
 		#contains every distinct value from a field
 		self.distinctList = list()
+		#number of distinct values
+		self.size = int()
 
 	def forgeDocSearch(self):
-		#size=0 means "no limit to the number of terms"
 		self.docSearch = {
+			"size": 0,
 			"aggs": {
 				"distinct": {
 					"terms": {
 						"field": self.field,
-						"size": 0
+						"size": self.size
 					}
 				}
 			}
 		}
+
+
+
+	def forgeDocSearchSize(self):
+		self.docSearch = {
+			"query": {
+				"bool": {
+					"must": [
+						{"exists": {"field": self.field}}
+					]
+				}
+			},
+			"aggs": {
+				"distinct": {
+					"cardinality": {
+						"field": self.field,
+					}
+				}
+			}
+		}
+
 
 	def search(self):
 		#request_timeout value to avoid ConnectionTimeout exception
@@ -46,39 +69,47 @@ class Field:
 	def processMatchResponse(self):
 		'''
 		raw matchResponse looks like:
-		{
-		    "_shards": {
-		        "failed": 0,
-		        "successful": 5,
-		        "total": 5
-		    },
-		    "aggregations": {
-		        "distinct": {
-		            "buckets": [
-		                {
-		                    "doc_count": 3,
-		                    "key": "http://larlingslotsen.se/wp-includes/css/tracking.php?l=_JeHFUq_VJOXK0QWHtoGYDw_Product-UserID&amp;userid=abuse@huawei.com"
-		                },
-		                {
-		                    "doc_count": 2,
-		                    "key": "http://aaronmaxdesign.com/wordpress/wp-content/netvigator/update/ainput/id.php?l=_JeHFUq_VJOXK0QWHtoGYDw_Product-UserID&amp;userid=abuse@aol.com"
-		                },
-		                {
-                    "doc_count": 15882,
-                    "key": 1359248160,
-                    "key_as_string": "81.4.123.32"
-                },
+
+		{u'_shards': {u'failed': 0, u'successful': 5, u'total': 5},
+		 u'aggregations': {u'distinct': {u'buckets': [{u'doc_count': 1,
+		                                               u'key': u'192.168.1.67'}],
+		                                 u'doc_count_error_upper_bound': 0,
+		                                 u'sum_other_doc_count': 0}},
+		 u'hits': {u'hits': [], u'max_score': 0.0, u'total': 4},
+		 u'timed_out': False,
+		 u'took': 9}
 
 		'''
-		if self.field != 'ip':
-			for element in self.matchResponse['aggregations']['distinct']['buckets']:
-				self.distinctList.append(element['key'])
-		else:
-			#if we are looking for all ips, the key is different
-			for element in self.matchResponse['aggregations']['distinct']['buckets']:
-				self.distinctList.append(element['key_as_string'])
+
+		for element in self.matchResponse['aggregations']['distinct']['buckets']:
+			self.distinctList.append(element['key'])
+
+	def getSize(self):
+		self.forgeDocSearchSize()
+		self.search()
+		'''
+		matchResponse looks like:
+
+		{u'_shards': {u'failed': 0, u'successful': 5, u'total': 5},
+		 u'aggregations': {u'distinct': {u'value': 446}},
+		 u'hits': {u'hits': [{u'_id': u'AVmJp90mcUY7Hd0SG5cP',
+		                      u'_index': u'hippocampe',
+		                      u'_score': 1.0,
+		                      u'_source': {u'domain': u'03bbec4.netsolhost.com',
+		                                   u'firstAppearance': u'20170110T193316+0100',
+		                                   u'idSource': u'AVmJp9c2cUY7Hd0SG5VN',
+		                                   u'lastAppearance': u'20170110T193316+0100',
+		                                   u'source': u''},
+		'''
+		self.size = self.matchResponse['aggregations']['distinct']['value']
+
+
 
 	def getDistinct(self):
+		#since Elasticsearch does not allow the "size=0" parameters
+		#we must get the number of all distinct doc according a specific field
+		self.getSize()
+		#then we execute the search with the exact size
 		self.forgeDocSearch()
 		self.search()
 		self.processMatchResponse()
